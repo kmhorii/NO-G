d496609fa51f8ef82fc7d1684d5e63619736be70
Bryan Dang <bdang@chapman.edu>
2019-10-02 09:34:37 -0700

    Adding Scripts folder/Starting AmmoUI

---
+++ No-G Unity/Assets/Scripts/UI Scripts/AmmoUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AmmoUI : MonoBehaviour
+{
+
+    public float fireRate = 15f;
+
+    public int maxAmmo = 10;
+    private int currentAmmo;
+
+    public float reloadTime = 1f;
+
+    private float nextTimetoFire = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextTimetoFire)
+        {
+            nextTimetoFire = Time.time + 1f / fireRate;
+
+        }
+    }
+
+   
+}
