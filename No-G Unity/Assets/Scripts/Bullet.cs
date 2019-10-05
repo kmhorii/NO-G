86802a0a82c4f18e506ef44eedc8fafb80a1d149
Kevin Horii <horii100@mail.chapman.edu>
2019-10-05 02:10:12 -0700

    Added shooting and bullet assets, also standard assets

---
+++ No-G Unity/Assets/Scripts/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public ShootingGun gunReference;
+
+    public int bounceNumber;
+
+    void Start()
+    {
+        gunReference = GameObject.FindGameObjectWithTag("Gun").GetComponent<ShootingGun>();
+        bounceNumber = gunReference.maxBounces;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+    //    transform.Translate(Vector3.forward * speed);
+
+    //    Ray ray = new Ray(transform.position, transform.forward);
+    //    RaycastHit hit;
+
+    //    if (Physics.Raycast(ray, out hit, Time.deltaTime * speed))
+    //    {
+    //        Vector3 reflectDirection = Vector3.Reflect(ray.direction, hit.normal);
+    //    }
+    //}
+
+    //private void OnTriggerEnter(Collider collider)
+    //{
+    //    if (bounceNumber == 0)
+    //    {
+    //        Destroy(gameObject);
+    //    }
+
+    //    bounceNumber--;
+    //}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (bounceNumber - 1 == 0)
+        {
+            Destroy(gameObject);
+        }
+
+        bounceNumber--;
+    }
+}
