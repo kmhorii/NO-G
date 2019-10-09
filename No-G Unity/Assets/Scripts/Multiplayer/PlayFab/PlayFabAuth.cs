c21fd5b1038f206e65835602844fde757fbc5b98
Theo Balestra <theobalestra@me.com>
2019-10-09 01:05:57 -0700

    Login

---
+++ No-G Unity/Assets/Scripts/Multiplayer/PlayFab/PlayFabAuth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayFabAuth : MonoBehaviour
+{
+	public InputField username;
+	public InputField password;
+	public string levelToLoad;
+
+    public void Login()
+	{
+		LoginWithPlayFabRequest request = new LoginWithPlayFabRequest();
+
+		request.Username = username.text;
+		request.Password = password.text;
+
+		PlayFabClientAPI.LoginWithPlayFab(request, result => {
+			Alerts a = new Alerts();
+			StartCoroutine(a.CreateNewAlert(username.text + " You have logged in!"));
+			//SceneManager.LoadScene(levelToLoad);
+
+		}, error => {
+			Alerts a = new Alerts();
+			StartCoroutine(a.CreateNewAlert(error.ErrorMessage));
+		});
+	}
+}
---
+++ No-G Unity/Assets/Scripts/Multiplayer/PlayFab/Register.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine.UI;
+
+public class Register : MonoBehaviour
+{
+	public InputField username;
+	public InputField password;
+	public InputField confPassword;
+	public InputField email;
+
+    public void CreateAccount()
+	{
+		if(password.text == confPassword.text)
+		{
+			RegisterPlayFabUserRequest request = new RegisterPlayFabUserRequest();
+			request.Username = username.text;
+			request.Password = confPassword.text;
+			request.Email = email.text;
+			request.DisplayName = username.text;
+
+			PlayFabClientAPI.RegisterPlayFabUser(request, result => {
+				Alerts a = new Alerts();
+				StartCoroutine(a.CreateNewAlert(result.Username + " has been created"));
+			}, error => {
+				Alerts a = new Alerts();
+				StartCoroutine(a.CreateNewAlert(error.ErrorMessage));
+			});
+		}
+	}
+}
