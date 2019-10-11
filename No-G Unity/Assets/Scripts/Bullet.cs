c21d638c549d825f4d79d466ba67c7646ebb10a8
Kevin <horii100@mail.chapman.edu>
2019-10-11 09:52:58 -0700

    Auto stash before merge of "master" and "origin/master"
    started bullet damage stuff

--- No-G Unity/Assets/Scripts/Bullet.cs
+++ No-G Unity/Assets/Scripts/Bullet.cs
@@ -7,16 +7,22 @@ public class Bullet : MonoBehaviour
     public ShootingGun gunReference;
 
     public int bounceNumber;
+    private int maxBounces;
+
+    public int playerHealth;
+
+    public int defaultDamage = 34;
+    public int damageReduction = 2;
 
     void Start()
     {
         gunReference = GameObject.FindGameObjectWithTag("Gun").GetComponent<ShootingGun>();
         bounceNumber = gunReference.maxBounces;
+        maxBounces = bounceNumber;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-
         if (collision.gameObject.layer == 9)
         {
             if (bounceNumber - 1 == 0)
@@ -26,5 +32,16 @@ public class Bullet : MonoBehaviour
 
             bounceNumber--;
         }
-    }
+
+        if (collision.gameObject.layer == 11)
+        {
+            if (bounceNumber <= maxBounces - 1)
+            {
+                //collision.gameObject.GetComponent<Player>().playerHealth;
+                int bulletDamage = defaultDamage - (damageReduction * (maxBounces - bounceNumber));
+            }
+        }
+
+
+    } 
 }
